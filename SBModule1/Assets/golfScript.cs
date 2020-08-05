1197a9db32c3ccdeacd8048bc292863b9a9669c9
Fartlicker777 <62307787+Fartlicker777@users.noreply.github.com>
2020-08-05 04:03:35 -0500

    Fixed everything

--- SBModule1/Assets/golfScript.cs
+++ SBModule1/Assets/golfScript.cs
@@ -27,21 +27,20 @@ public class golfScript : MonoBehaviour {
     int Input = 0;
     float GolfBallDistance = 0.0767f;
     private List<int> ActualWindDirection = new List<int> {22, 45, 67, 90, 112, 135, 157, 180, 202, 225, 247, 270, 292, 315, 337, 360};
-    private List<float> ArrowRotation = new List<float> {22.5f, 45f, 67.5f, 90f, 112.5f, 135f, 157.5f, 180f, 202.5f, 225f, 247.5f, 270f, 292.5f, 315f, 337.5f, 0f};
+    private List<float> ArrowRotation = new List<float> {0f, -22.5f, -45f, -67.5f, -90f, -112.5f, -135f, -157.5f, -180f, -202.5f, -225f, -247.5f, -270f, -292.5f, -315f, -337.5f};//For some reason it needed to start at 0f, it actually doesn't make sense
     private List<string> Terms = new List<string> {"Hole in One", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Triple Bogey", "+8", "+9", "+10", "+11"};
     private List<string> DirectionNames = new List<string> {"N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"};
-
-
-
 
     static int moduleIdCounter = 1;
     int moduleId;
     private bool moduleSolved;
 
-    void Awake() {
+    void Awake()
+    {
         moduleId = moduleIdCounter++;
 
-        foreach (KMSelectable Arrow in Arrowbuttons) {
+        foreach (KMSelectable Arrow in Arrowbuttons)
+        {
             Arrow.OnInteract += delegate () { ArrowPress(Arrow); return false; };
         }
         foreach (KMSelectable NumberButton in KeypadButtons)
@@ -52,8 +51,10 @@ public class golfScript : MonoBehaviour {
     }
 
 
-    void Start() {
-      for(int i = 0; i < 9; i++){
+    void Start()
+    {
+      for(int i = 0; i < 9; i++)
+      {
         HoleStrokes[i] = UnityEngine.Random.Range(1, 12);
       }
       WindDirection = UnityEngine.Random.Range(0,16);
@@ -62,7 +63,8 @@ public class golfScript : MonoBehaviour {
       WindDirection = ActualWindDirection[WindDirection];
       Handicap = (((Bomb.GetModuleNames().Count()-9)*(113/WindDirection)) + 5500) % 55;
       Debug.LogFormat("[Golf #{0}] The Handicap value starts at {1}.", moduleId, Handicap);
-      for(int i = 0; i < 9; i++){
+      for(int i = 0; i < 9; i++)
+      {
       Debug.LogFormat("[Golf #{0}] Hole {1} was a(n) {2}, adds {3} strokes.", moduleId, i+1, Terms[HoleStrokes[i]-1], HoleStrokes[i]);
       }
       Stroke.text = Terms[HoleStrokes[0]-1];
@@ -78,37 +80,50 @@ public class golfScript : MonoBehaviour {
     {
       SubmitButton.AddInteractionPunch();
       Audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress,SubmitButton.transform);
-      if (Input == Total){
+      if (moduleSolved)
+      {
+        return;
+      }
+      if (Input == Total)
+      {
         Debug.LogFormat("[Golf #{0}] You entered {1}, module solved!", moduleId, Total);
         GetComponent<KMBombModule>().HandlePass();
+        moduleSolved = true;
         StartCoroutine(ping());
       }
-      else{
+      else
+      {
         Debug.LogFormat("[Golf #{0}] You entered {1}, that is incorrect.", moduleId, Input);
         GetComponent<KMBombModule>().HandleStrike();
         Input &= 0;
         InputText.text = null;
       }
     }
-    void ArrowPress(KMSelectable ArrowCunt)
+
+    void ArrowPress(KMSelectable Arrow)
     {
-      ArrowCunt.AddInteractionPunch();
-      Audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress,ArrowCunt.transform);
-          if(Arrow == Arrowbuttons[0])
+      Arrow.AddInteractionPunch();
+      Audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress,Arrow.transform);
+          if (Arrow == Arrowbuttons[0])
             {
-              if (CurrentHole != 1){
-                CurrentHole -= 1;
-                Stroke.text = Terms[HoleStrokes[CurrentHole - 1]-1];
-                Hole.text = "Hole "+CurrentHole;
+              if (CurrentHole != 1)
+              {
+                CurrentHole--;
+                Stroke.text = Terms[HoleStrokes[CurrentHole - 1] - 1];
+                Hole.text = "Hole "+ CurrentHole;
               }
-            } else {
+            }
+            else
+            {
               if (CurrentHole != 9)
               {
-                CurrentHole += 1;
-                Stroke.text = Terms[HoleStrokes[CurrentHole - 1]-1];
-                Hole.text = "Hole "+CurrentHole; }
+                CurrentHole++;
+                Stroke.text = Terms[HoleStrokes[CurrentHole - 1] - 1];
+                Hole.text = "Hole "+ CurrentHole;
+              }
             }
     }
+
     void NumberPress(KMSelectable NumberButton)
     {
       NumberButton.AddInteractionPunch();
@@ -122,12 +137,69 @@ public class golfScript : MonoBehaviour {
             }
         }
     }
-    IEnumerator ping(){
+
+    IEnumerator ping()
+    {
       Audio.PlaySoundAtTransform("ping_soundeffect", transform);
         for(int i = 0; i < 200; i++){
         yield return new WaitForSeconds(0.02f);
         GolfBallDistance += 0.05f;
         GolfBall.transform.localPosition = new Vector3(0.0189f, -0.01462f, GolfBallDistance);
       }
+    }
+
+    #pragma warning disable 414
+    private readonly string TwitchHelpMessage = @"Use !{0} cycle to cycle between all of the holes. Use !{0} submit ## to submit a number.";
+    #pragma warning restore 414
+
+    IEnumerator ProcessTwitchCommand(string Command)
+    {
+      Command = Command.Trim();
+      string[] Parameters = Command.Split(' ');
+      if (Command.ToString().ToUpper() == "CYCLE")
+      {
+        yield return null;
+        for (int i = 0; i < 9; i++)
+        {
+          Arrowbuttons[1].OnInteract();
+          yield return new WaitForSeconds(1.5f);
+        }
+        yield return new WaitForSeconds(1.5f);
+        for (int i = 0; i < 9; i++)
+        {
+          Arrowbuttons[0].OnInteract();
+          yield return new WaitForSeconds(.5f);
+        }
+        yield break;
+      }
+      else if (Parameters[0].ToString().ToUpper() != "SUBMIT" || Parameters.Length != 2)
+      {
+        yield return null;
+        yield return "sendtochaterror Invalid command!";
+        yield break;
+      }
+      else if (Parameters[1].Length >= 8)
+      {
+        yield return null;
+        yield return "sendtochaterror Too big of a number!";
+        yield break;
+      }
+      for (int i = 0; i < Parameters.Length; i++)
+      {
+        if (Parameters[1][i].ToString() != "1" && Parameters[1][i].ToString() != "2" && Parameters[1][i].ToString() != "3" && Parameters[1][i].ToString() != "4"
+        && Parameters[1][i].ToString() != "5" && Parameters[1][i].ToString() != "6" && Parameters[1][i].ToString() != "7" && Parameters[1][i].ToString() != "8"
+        && Parameters[1][i].ToString() != "9" && Parameters[1][i].ToString() != "0")
+        {
+          yield return null;
+          yield return "sendtochaterror Invalid command!";
+          yield break;
+        }
+      }
+      for (int i = 0; i < Parameters.Length; i++)
+      {
+        KeypadButtons[int.Parse(Parameters[1][i].ToString())].OnInteract();
+        yield return new WaitForSeconds(.1f);
+      }
+      SubmitButton.OnInteract();
     }
  }
